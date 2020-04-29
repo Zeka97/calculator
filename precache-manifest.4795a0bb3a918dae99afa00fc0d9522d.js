e9992cf664723892dde2a24b20f5d4c384036e17
Amel <zekafrombtm@hotmail.com>
2020-04-29 17:21:06 +0200

    Updates

---
+++ precache-manifest.4795a0bb3a918dae99afa00fc0d9522d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f18f1cf28fb0735bde60ff52fe7176",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2ed8a9ab6b372cad8355",
+    "url": "/calculator/static/css/main.3282f103.chunk.css"
+  },
+  {
+    "revision": "a484dc6635d9eafd0a60",
+    "url": "/calculator/static/js/2.fcb17d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.fcb17d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed8a9ab6b372cad8355",
+    "url": "/calculator/static/js/main.f7ae4896.chunk.js"
+  },
+  {
+    "revision": "ca9f9d06e9f3d8684b33",
+    "url": "/calculator/static/js/runtime-main.1ad482a8.js"
+  }
+]);
